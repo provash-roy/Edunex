e2cc854c2526bc4985a33d7a94bb99eee0b51de0
Provash-roy <provashray94@gmail.com>
2026-04-17 22:36:46 +0600

    add title form with validation

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -1,6 +1,27 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import * as z from "zod";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Pencil } from "lucide-react";
-import React from "react";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldLabel,
+} from "@/components/ui/field";
+
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const schema = z.object({
+  title: z.string().min(5, "Title must be at least 5 characters"),
+});
 
 interface TitleFormProps {
   initialValues: {
@@ -10,15 +31,73 @@ interface TitleFormProps {
 }
 
 export default function TitleForm({ initialValues, courseId }: TitleFormProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  const toggleEdit = () => setIsEditing((prev) => !prev);
+
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      title: initialValues.title,
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof schema>) => {
+    const response = await axios.patch(`/api/courses${courseId}`, data);
+
+    toast.success("Course updated successfully!");
+  };
+
   return (
     <div className="border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
         Course Title
-        <Button variant="ghost" size="sm">
-          <Pencil/>
-          Edit Title
+        <Button onClick={toggleEdit} variant="ghost" size="sm">
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil />
+              Edit Title
+            </>
+          )}
         </Button>
       </div>
+      {!isEditing && (
+        <p className="text-sm text-slate-600 mt-1">{initialValues.title}</p>
+      )}
+      {isEditing && (
+        <>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <Controller
+              name="title"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name}>Course Title</FieldLabel>
+
+                  <Input
+                    {...field}
+                    id={field.name}
+                    aria-invalid={fieldState.invalid}
+                    placeholder="e.g. Complete MERN Stack Course"
+                    autoComplete="off"
+                  />
+
+                  <FieldDescription>
+                    Give your course a clear and descriptive name.
+                  </FieldDescription>
+
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Button type="submit">Submit</Button>
+          </form>
+        </>
+      )}
     </div>
   );
 }
