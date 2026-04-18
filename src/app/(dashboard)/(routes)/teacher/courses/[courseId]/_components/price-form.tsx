f19dc2306a1cd62e299b0e5fe9144477855eed14
Provash-roy <provashray94@gmail.com>
2026-04-18 23:58:52 +0600

    add price form

---
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/price-form.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import * as z from "zod";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldLabel,
+} from "@/components/ui/field";
+
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import toast from "react-hot-toast";
+//price form
+
+const schema = z.object({
+  price: z.number().positive("Price must be a positive number"),
+});
+
+interface PriceFormProps {
+  initialValues: {
+    price: number;
+  };
+  courseId: string;
+}
+
+export default function PriceForm({ initialValues, courseId }: PriceFormProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  const toggleEdit = () => setIsEditing((prev) => !prev);
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      price: initialValues.price,
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (data: z.infer<typeof schema>) => {
+    await axios.patch(`/api/courses/${courseId}`, data);
+    toast.success("Course updated");
+    toggleEdit();
+    router.refresh();
+  };
+
+  return (
+    <div className="border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Sell your course
+        <Button onClick={toggleEdit} variant="ghost" size="sm">
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil />
+              Edit Price
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <p className="text-sm text-slate-600 mt-1">${initialValues.price.toFixed(2)}</p>
+      )}
+      {isEditing && (
+        <>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <Controller
+              name="price"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name}>Course Price</FieldLabel>
+
+                  <Input
+                    {...field}
+                    disabled={isSubmitting}
+                    id={field.name}
+                    aria-invalid={fieldState.invalid}
+                    placeholder="e.g. 29.99"
+                    autoComplete="off"
+                  />
+
+                  <FieldDescription>
+                    Set a price for your course.
+                  </FieldDescription>
+
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Button type="submit">Submit</Button>
+          </form>
+        </>
+      )}
+    </div>
+  );
+}
--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -7,6 +7,7 @@ import { LayoutDashboard } from "lucide-react";
 import IconBadge from "./_components/icon-badge";
 import TitleForm from "./_components/title-form";
 import DescriptionForm from "./_components/description-form";
+import PriceForm from "./_components/price-form";
 
 interface CourseIdPageProps {
   params: Promise<{ courseId: string }>;
@@ -45,6 +46,7 @@ export default async function CourseIdPage({ params }: CourseIdPageProps) {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
         <TitleForm initialValues={course} courseId={courseId} />
         <DescriptionForm initialValues={course} courseId={courseId} />
+        <PriceForm initialValues={course} courseId={courseId} />
         <TitleForm initialValues={course} courseId={courseId} />
         <TitleForm initialValues={course} courseId={courseId} />
         <TitleForm initialValues={course} courseId={courseId} />
@@ -53,3 +55,4 @@ export default async function CourseIdPage({ params }: CourseIdPageProps) {
     </div>
   );
 }
+``;
